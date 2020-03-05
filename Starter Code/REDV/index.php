615fe79ec65439a3ce978adbf0cdc7c2fd3624a3
SiyuanWang16156333 <61172033+SiyuanWang16156333@users.noreply.github.com>
2020-03-05 11:04:50 +0800

    Create index.php

---
+++ Starter Code/REDV/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Required mata tags -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+  <!--Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>Red Velvet</title>
+</head>
+<body>
+
+<div class="container">
+  <div class="logo">
+    <a href="index2.php"><img class="icon" src="images/7.jpg" alt="Icon" width="150px"></a>
+  </div>
+
+  <div class="row">
+     <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+    <section>
+      <img class="icon" src="images/1.JPG" alt="Icon" width="150px">
+      <h3>Red Velvet</h3>
+      <p>Red velvet is a women's singing group launched by South Korea SM Entertainment Co., Ltd. in August 2014.</p>
+    </section>
+    </div>
+
+     <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+    <section>
+      <img class="icon" src="images/2.JPG" alt="Icon" width="150px">
+      <h3>Yeri</h3>
+      <p>Selected as a trainee of SM company through South Korea's "S.M. casting system" draft.</p>
+    </section>
+  </div>
+
+
+    <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+    <section>
+      <img class="icon" src="images/3.JPG" alt="Icon" width="150px">
+      <h3>SeulGi</h3>
+      <p>In 2007, Korean SM Entertainment Co., Ltd. was selected to become a trainee.</p>
+    </section>
+  </div>
+
+
+     <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+    <section>
+      <img class="icon" src="images/4.JPG" alt="Icon" width="150px">
+      <h3>Irene</h3>
+      <p>In 2009, the talent show of South Korea SM Entertainment Co., Ltd. was discovered and officially entered the company to become a trainee.</p>
+    </section>
+    </div>
+
+    <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+    <section>
+      <img class="icon" src="images/5.JPG" alt="Icon" width="150px">
+      <h3>Joy</h3>
+      <p>She was a member of the group who did not appear through SM rocks. She became an intern through the selection of SM Entertainment in Seoul in 2012.</p>
+    </section>
+  </div>
+
+
+     <div class="col-12 col-sm-12 col-md-6 col-lg-4">
+    <section>
+      <img class="icon" src="images/6.JPG" alt="Icon" width="150px">
+      <h3>Wendy</h3>
+      <p>In 2012, she became a trainee of SM Entertainment in Canada.</p>
+    </section>
+    </div>
+  </div><!-- row -->
+</div><!-- content container -->
+
+
+
+
+
+
+
+
+<!--Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
